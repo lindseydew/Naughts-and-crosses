a8abd1c6dd5f074889b99789cf40e3cd560d2d6f
Lindsey Dew <lindsey.dew@guardian.co.uk>
2011-11-19 18:11:21 +0000

    changes made, still need to finish

--- naughtsandcrosses.scala
+++ naughtsandcrosses.scala
@@ -3,9 +3,10 @@ import scala.collection.mutable.Map
 case class Coord (x: Int, y: Int)
 
 
-var board: Map[Coord, Option[String]] = Map(Coord(0,0) -> None, Coord(0,1) -> None, Coord(0,2) -> None, Coord(1,0) -> None, Coord(1,1) -> None, Coord(1,2) -> None, Coord(2,0) -> None, Coord(2,1) -> None, Coord(2,2) -> None)
+var board: Map[Coord, Option[Char]] = Map(Coord(0,0) -> None, Coord(0,1) -> None, Coord(0,2) -> None, Coord(1,0) -> None, Coord(1,1) -> None, Coord(1,2) -> None, Coord(2,0) -> None, Coord(2,1) -> None, Coord(2,2) -> None)
 printBoard
-board.put(Coord(1,2), Some("X"))
+printBoardWithNumbers
+/*board.put(Coord(1,2), Some("X"))
 printBoard
 
 board.put(Coord(0,2), Some("O"))
@@ -15,17 +16,57 @@ board.put(Coord(1,1), Some("X"))
 printBoard
 
 board.put(Coord(1,0), Some("O"))
-printBoard 
+printBoard */
+var turn: Char = 'X'
 
+while (!isGameOver) {
+  printBoard
+  println("Enter a number between 1 and 9 to start your go")
+  var input = Console.readInt
+  if (input < 1 || input > 9) {
+    println("You fucking idiot, try again")
+  }
+  var xAxis = (input - 1) / 3
+  var yAxis = (input - 1) % 3
+
+  board.put(Coord(xAxis, yAxis), Some(turn))
+  printBoard
+  println()
+  if (turn == 'X') turn = 'O'
+  else turn = 'X'
+}
 
 def printBoard = { 
-	println("********")
-	for {
-		x <- 0 to 2
-		y <- 0 to 2
-	} {
-		print(board(Coord(x,y)) getOrElse " ")
-		if(y == 2) println()
-		else print("|")
+  println("********")
+  for {
+      x <- 0 to 2
+      y <- 0 to 2
+    } {
+    print(board(Coord(x,y)) getOrElse " ")
+    if(y == 2) println()
+    else print("|")
   }
+}
+
+def printBoardWithNumbers = {
+  println("**********")
+  var i = 1  
+  for {
+      x <- 0 to 2
+      y <- 0 to 2
+    } {
+    print(i)
+    i += 1  
+    if(y == 2) println()
+    else print("|")
+    
+  }
+}
+
+def isGameOver = {
+  false
+}
+
+def gameResult = {
+  
 }
